5f846e17a99d1cb7f29a550f31144dead1e36cce
Borja <alu0100498820@ull.edu.es>
2015-10-14 19:20:13 +0100

    Modificando test y añadiendo chai.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,34 +1,42 @@
-var assert = chai.assert;
+var expect = chai.expect;
 
-suite('temperature', function() {
-    test('32F = 0C', function() {
-        original.value = "32F";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+describe("__ TEST BDD __", function() {
+
+
+
+  it("32F", function() {
+    var temp = new Temperatura(32,"F");
+    expect(temp.get_valor()).to.equal(32);
+    expect(temp.get_tipo()).to.equal("F");
+  });
+
+  it("45C", function() {
+    var temp = new Temperatura(45,"C");
+    expect(temp.get_valor()).to.equal(45);
+    expect(temp.get_tipo()).to.equal("C");
+  });
+
+  it("5X = ERROR", function() {
+      window.onload = function() {
+        var temp = new Temperatura(5,0,"X");
+        convertir();
+        expect(fin.innerHTML).to.match("/ERROR/");
+      }
     });
-    test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
-    });
-    test('4e2F = 204.4C', function() {
-        original.value = "4e2F";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "204.4 Celsius");
-    });
-    test('-11e-7C = 32.0F', function() {
-        original.value = "-11e-7C";
-        calculate();
-        assert.deepEqual(converted.innerHTML,"32.0 Farenheit");
-    });
-    test('3.3e-2C Es un numero?', function() {
-       original.value = "3.3e-2C";
-       calculate();
-       assert.isString(converted.innerHTML);
-    });
-    test('5X = error', function() {
-        original.value = "5X";
-        calculate();
-        assert.match(converted.innerHTML, /ERROR/);
-    });
+
+  it("2.34C = 36.212F", function() {
+    var temp = new Temperatura();
+    temp.set_valor(2.34);
+    temp.set_tipo("C");
+    var result = temp.pasar_a_f();
+    expect(result).to.equal(36.212);
+  });
+
+  it("32F = 0C", function() {
+    var temp = new Temperatura();
+    temp.set_valor(32);
+    temp.set_tipo("F");
+    var result = temp.pasar_a_c();
+    expect(result).to.equal(0);
+  });
 });
